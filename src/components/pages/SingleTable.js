66dcc4b78ff4d5eb18261f79d41845f361174208
malgorzataziolek <gosiaziolek1223@gmail.com>
2022-05-16 15:23:47 +0200

    init project

---
+++ src/components/pages/SingleTable.js
@@ -0,0 +1,5 @@
+const SingleTable = () => {
+	return <h2>Single table</h2>;
+};
+
+export default SingleTable;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import React from 'react';
+import store from './redux/store';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>,
+
+	document.querySelector('#root')
+);
